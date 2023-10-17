cbee7dfb046cca6642ee0e9c0ba9f805dd50fec9
Matteo Conti <matconti06@gmail.com>
2023-10-17 13:03:08 +0200

    aggiunta attributi + get e set

--- Automobile/Class1.cs
+++ Automobile/Class1.cs
@@ -8,6 +8,32 @@ namespace Automobile
 {
     internal class Auto
     {
+        //attributi
+        private bool motore;//dichiaro la variabile utilizzata per l'accensione dell'auto
+        private int tacchimetro;//dichiaro variable per la visualizzazione della velocità
+        private int cambio;//dichiaro variabile per la visualizzazione della marcia
 
+        public Auto()
+        {
+            Motore = false;
+            Tacchimetro = 0;
+            Cambio = 0;
+        }
+        //funzioni get e set
+        bool Motore
+        {
+            get { return motore; }
+            set { motore = value; }
+        }
+        int Tacchimetro
+        {
+            get { return tacchimetro; }
+            set { tacchimetro = value; }
+        }
+        int Cambio
+        {
+            get { return cambio; }
+            set { cambio = value; }
+        }
     }
 }
